38888a94eff2ea0495cdc7636b8e0ac2cb330e41
andy-thomason <andy@andythomason.com>
2021-01-17 21:24:43 +0000

    Add comments to extendrtests.

--- tests/extendrtests/R/wrappers.R
+++ tests/extendrtests/R/wrappers.R
@@ -1,11 +1,19 @@
 #' Wrappers for Rust test functions.
 #' 
-#' Wrappers for Rust test functions.
 #' @rdname wrappers
 #' @export
 hello_world <- function() {
   .Call(wrap__hello_world)
 }
 
+#' Get module metadata as a list.
+#' 
+#' @rdname wrappers
+#' @export
 get_extendrtests_metadata <- function() .Call(wrap__get_extendrtests_metadata)
+
+#' Build wrappers 
+#' 
+#' @rdname wrappers
+#' @export
 make_extendrtests_wrappers <- function(use_symbols=TRUE) .Call(wrap__make_extendrtests_wrappers, use_symbols)
